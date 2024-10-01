133f24a9f15041a73d3a67f3cf5fa3af947fdbc0
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-09-30 21:41:39 -0300

    add deduct operation done

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { useState } from 'react';
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0');
   const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleOnClear = () => {
     setCurrentNumber('0')
     setFirstNumber('0')
+    setOperation('')
   }
 
   const handleAddNumber = (num) => {
@@ -20,9 +22,39 @@ const App = () => {
     if(firstNumber === '0'){
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0')
+      setOperation('+')
     }else{
       const sum = Number(firstNumber) + Number(currentNumber);
       setCurrentNumber(String(sum));
+      setOperation('')
+    }
+  }
+
+  const handleDeductNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('-')
+    }else{
+      const deduct = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(deduct));
+      setOperation('')
+    }
+  }
+
+  const handleEquals = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          handleDeductNumbers();
+          break;
+        default:
+          handleOnClear();
+          break;
+      }
     }
   }
 
@@ -40,7 +72,7 @@ const App = () => {
           <Button label="7" onClick={()=>handleAddNumber('7')}/>
           <Button label="8" onClick={()=>handleAddNumber('8')}/>
           <Button label="9" onClick={()=>handleAddNumber('9')}/>
-          <Button label="-" onClick={()=>handleAddNumber('')}/>
+          <Button label="-" onClick={handleDeductNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={()=>handleAddNumber('4')}/>
@@ -52,7 +84,7 @@ const App = () => {
           <Button label="1" onClick={()=>handleAddNumber('1')}/>
           <Button label="2" onClick={()=>handleAddNumber('2')}/>
           <Button label="3" onClick={()=>handleAddNumber('3')}/>
-          <Button label="=" onClick={()=>handleAddNumber('=')}/>
+          <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
     </Container>
